f63510a905f18cff926992149e2a1848730c8f8c
Nandan54321 <nandanlive34@gmail.com>
2026-04-20 16:59:51 +0530

    deleted some files and changed few

--- resume-recommender/db/mongo.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-import os
-
-MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI)
-db = client["resume_db"]
-
-resumes_collection = db["resumes"]
-jobs_collection = db["jobs"]
